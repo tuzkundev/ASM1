233106578a6ab53d1aa7188336cc01d14adf7410
Nguyễn Quốc Quân <99194094+tuzkundev@users.noreply.github.com>
2022-04-27 22:21:06 +0700

    React Redux Form Validation lab07.3

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,16 +3,19 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   Button,
-  Form,
-  FormGroup,
   Label,
-  Input,
   Col,
   Row,
-  FormFeedback,
 } from "reactstrap";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+const isNumber = (val) => !isNaN(Number(val));
+const validEmail = (val) =>
+  /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends Component {
   constructor(props) {
@@ -32,26 +35,26 @@ class Contact extends Component {
         email: false,
       },
     };
-    this.handleInputChange = this.handleInputChange.bind(this);
+    // this.handleInputChange = this.handleInputChange.bind(this);
     this.handleInputSubmit = this.handleInputSubmit.bind(this);
-    this.handleBlur = this.handleBlur.bind(this);
+    // this.handleBlur = this.handleBlur.bind(this);
   }
 
-  handleBlur = (field) => (evt) => {
-    this.setState({
-      touched: { ...this.state.touched, [field]: true },
-    });
-  };
+  // handleBlur = (field) => (evt) => {
+  //   this.setState({
+  //     touched: { ...this.state.touched, [field]: true },
+  //   });
+  // };
 
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
+  // handleInputChange(event) {
+  //   const target = event.target;
+  //   const value = target.type === "checkbox" ? target.checked : target.value;
+  //   const name = target.name;
 
-    this.setState({
-      [name]: value,
-    });
-  }
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // }
 
   handleInputSubmit(values) {
     console.log("Current state is: " + JSON.stringify(this.state));
@@ -59,44 +62,44 @@ class Contact extends Component {
     //event.preventDefault();
   }
 
-  validate(firstname, lastname, telnum, email) {
-    const errors = {
-      firstname: "",
-      lastname: "",
-      telnum: "",
-      email: "",
-    };
+  // validate(firstname, lastname, telnum, email) {
+  //   const errors = {
+  //     firstname: "",
+  //     lastname: "",
+  //     telnum: "",
+  //     email: "",
+  //   };
 
-    if (this.state.touched.firstname && firstname.length < 3)
-      errors.firstname = "First Name should be >= 3 characters";
-    else if (this.state.touched.firstname && firstname.length > 10)
-      errors.firstname = "First Name should be <= 10 characters";
+  //   if (this.state.touched.firstname && firstname.length < 3)
+  //     errors.firstname = "First Name should be >= 3 characters";
+  //   else if (this.state.touched.firstname && firstname.length > 10)
+  //     errors.firstname = "First Name should be <= 10 characters";
 
-    if (this.state.touched.lastname && lastname.length < 3)
-      errors.lastname = "Last Name should be >= 3 characters";
-    else if (this.state.touched.lastname && lastname.length > 10)
-      errors.lastname = "Last Name should be <= 10 characters";
+  //   if (this.state.touched.lastname && lastname.length < 3)
+  //     errors.lastname = "Last Name should be >= 3 characters";
+  //   else if (this.state.touched.lastname && lastname.length > 10)
+  //     errors.lastname = "Last Name should be <= 10 characters";
 
-    const reg = /^\d+$/;
-    if (this.state.touched.telnum && !reg.test(telnum))
-      errors.telnum = "Tel. Number should contain only numbers";
+  //   const reg = /^\d+$/;
+  //   if (this.state.touched.telnum && !reg.test(telnum))
+  //     errors.telnum = "Tel. Number should contain only numbers";
 
-    if (
-      this.state.touched.email &&
-      email.split("").filter((x) => x === "@").length !== 1
-    )
-      errors.email = "Email should contain a @";
+  //   if (
+  //     this.state.touched.email &&
+  //     email.split("").filter((x) => x === "@").length !== 1
+  //   )
+  //     errors.email = "Email should contain a @";
 
-    return errors;
-  }
+  //   return errors;
+  // }
 
   render() {
-    const errors = this.validate(
-      this.state.firstname,
-      this.state.lastname,
-      this.state.telnum,
-      this.state.email
-    );
+    // const errors = this.validate(
+    //   this.state.firstname,
+    //   this.state.lastname,
+    //   this.state.telnum,
+    //   this.state.email
+    // );
 
     return (
       <div className="container">
@@ -176,6 +179,21 @@ class Contact extends Component {
                     name="firstname"
                     placeholder="First Name"
                     className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15),
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".firstname"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                      minLength: "Must be greater than 3 characters",
+                      maxLength: "Must be 15 characters or less",
+                    }}
                   />
                 </Col>
               </Row>
@@ -190,6 +208,21 @@ class Contact extends Component {
                     name="lastname"
                     placeholder="Last Name"
                     className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15),
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".lastname"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                      minLength: "Must be greater than 2 characters",
+                      maxLength: "Must be 15 characters or less",
+                    }}
                   />
                 </Col>
               </Row>
@@ -204,6 +237,23 @@ class Contact extends Component {
                     name="telnum"
                     placeholder="Tel. Number"
                     className="form-control"
+                    validators={{
+                      required,
+                      minLength: minLength(3),
+                      maxLength: maxLength(15),
+                      isNumber,
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".telnum"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                      minLength: "Must be greater than 2 characters",
+                      maxLength: "Must be 15 characters or less",
+                      isNumber: "Must be a number",
+                    }}
                   />
                 </Col>
               </Row>
@@ -218,6 +268,19 @@ class Contact extends Component {
                     name="email"
                     placeholder="Email"
                     className="form-control"
+                    validators={{
+                      required,
+                      validEmail,
+                    }}
+                  />
+                  <Errors
+                    className="text-danger"
+                    model=".email"
+                    show="touched"
+                    messages={{
+                      required: "Required",
+                      validEmail: "Invalid Email Address",
+                    }}
                   />
                 </Col>
               </Row>
